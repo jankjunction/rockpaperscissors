e2be11b93a8f800607bfa9afd8af35758e09c76e
Micah Bell <micahjoshuabell@gmail.com>
2022-04-21 12:29:42 -0400

    ADD Styling

--- script.js
+++ script.js
@@ -10,50 +10,27 @@ const paperDiv = document.createElement('div');
 const scissorsDiv = document.createElement('div');
 const resultsDiv = document.createElement('div');
 
-scoreDiv.textContent = 'Your Score: ' + playerScore + `   Computer Score: ` + computerScore;
-rockDiv.textContent = 'rock';
-paperDiv.textContent = 'paper';
-scissorsDiv.textContent = 'scissors';
-
+scoreDiv.textContent = `Your Score: ${playerScore}          Computer Score: ${computerScore}`;
+rockDiv.textContent = 'Rock';
+paperDiv.textContent = 'Paper';
+scissorsDiv.textContent = 'Scissors';
 
 gameDiv.appendChild(scoreDiv);
 gameDiv.appendChild(choicesDiv);
 gameDiv.appendChild(resultsDiv);
 choicesDiv.append(rockDiv, paperDiv, scissorsDiv);
 
+rockDiv.classList.add('choice');
+paperDiv.classList.add('choice');
+scissorsDiv.classList.add('choice');
+choicesDiv.classList.add('choices');
+resultsDiv.classList.add('results');
+scoreDiv.classList.add('score');
 
 
+rockDiv.style.backgroundColor = 'hsla(23, 69%, 50%, 1)';
+scissorsDiv.style.backgroundColor = 'silver';
 
-// Game Styling
-gameDiv.style.maxWidth = '800px';
-gameDiv.style.justifyContent = 'center';
-
-// Score Styling
-scoreDiv.style.display = 'flex';
-scoreDiv.style.justifyContent = 'center';
-
-// Choice Styling
-choicesDiv.style.display = 'flex';
-choicesDiv.style.paddingTop = '20px';
-choicesDiv.style.paddingLeft = '60px';
-choicesDiv.style.paddingRight = '60px';
-choicesDiv.style.justifyContent = 'space-between';
-
-
-rockDiv.style.cursor = 'default';
-paperDiv.style.cursor = 'default';
-scissorsDiv.style.cursor = 'default';
-
-// choicesDiv.array.forEach(element => {
-//     element.style.cursor = 'default';
-// });
-
-// Results Styling
-resultsDiv.style.display = 'flex';
-resultsDiv.style.paddingTop = '20px';
-resultsDiv.style.paddingLeft = '60px';
-resultsDiv.style.paddingRight = '60px';
-resultsDiv.style.justifyContent = 'center';
 
 // Event Listening
 
@@ -61,8 +38,6 @@ rockDiv.addEventListener('click', () => {
     playRound('rock', computerPlay());
     scoring();
 });
-
-
 
 paperDiv.addEventListener('click', () => {
     playRound('paper', computerPlay());
@@ -72,22 +47,18 @@ paperDiv.addEventListener('click', () => {
 scissorsDiv.addEventListener('click', () => {
     playRound('scissors', computerPlay());
     scoring();
-
 });
 
 
 function scoring () {
     resultsDiv.textContent = result;
-    if (result === 'tie') {scoreDiv.textContent = 'Your Score: ' + playerScore + `   Computer Score: ` + computerScore; return;};
+    if (result === 'tie') {scoreDiv.textContent = `Your Score: ${playerScore}    Computer Score: ${computerScore}`; return;};
     (result === 'win') ? playerScore++ : computerScore++
-    (playerScore >= 5) ? scoreDiv.textContent = 'You Win!': scoreDiv.textContent = 'Your Score: ' + playerScore + `   Computer Score: ` + computerScore;
+    (playerScore >= 5) ? scoreDiv.textContent = 'You Win!': scoreDiv.textContent = `Your Score: ${playerScore}    Computer Score: ${computerScore}`;
     if (computerScore >= 5) {scoreDiv.textContent = 'You Lose!';}
 };
 
 //computerPlay  Need to create a function that will randomly return Rock Paper or Scissors.  
-
-
-
 function computerPlay () {
     // computerChoice returns a choice of 'rock' 'paper' or 'scissors'
     let computerChoice = Math.floor(Math.random() * 3) + 1;
@@ -99,7 +70,6 @@ function computerPlay () {
 };
 
 //playRound  Create a function that takes the input playerSelection and computerSelection and determines a winner or if the round tied.
-
 function playRound (playerSelection, computerSelection) {
 
     if (playerSelection === computerSelection) {
@@ -110,32 +80,3 @@ function playRound (playerSelection, computerSelection) {
     return result;
 
 };
-
-//game() Create a function that calls playRound 5 times and keeps score and reports a winner at the end.
-
-// function game() {
-//     let playerScore = 0;
-//     let computerScore = 0;
-//     for (let i = 0; i < 5; i++) {
-//         const playerSelection = prompt('Please enter Rock, Paper or Scissors', 'rock').toLowerCase();
-//         const computerSelection = computerPlay();
-
-//         let result = playRound(playerSelection, computerSelection);
-       
-//         if (result === 'win') {
-//             playerScore += 1;
-//         } else if (result === 'lose') {
-//             computerScore += 1;
-//         } else 
-//             playerScore += 0;
-//             computerScore += 0;
-//     }
-//     console.log(playerScore);
-//     console.log(computerScore);
-   
-//     playerScore > computerScore ? console.log('You Win') : console.log('You Lose');
-// };
-
-
-
-// game();
